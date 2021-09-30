5671a0a7e40c67eb6a4c77993e0da46f0baa5dc9
﻿OhHyeonJi3088 <orange3088@gmail.com>
2021-09-30 13:09:51 +0900

    TeamMemberOhj merge하기 전 내용변경

--- PublicGitTest/src/team/member/ohj/TeamMemberOhj.java
+++ PublicGitTest/src/team/member/ohj/TeamMemberOhj.java
@@ -5,7 +5,7 @@ public class TeamMemberOhj {
 	public static void main(String[] args) {
 		// 팀원인 ohj 전용 브랜치인 OhHyeonJi 브랜치에서 작업한 것임.
 		
-		System.out.println("### 팀원인 ohj 가 작업한 것 ###");
+		System.out.println("### 팀원인 ohj 가 작업한 것 ###"); 
 
 	}
 
