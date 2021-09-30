d000f9df71183b3345aa9fee953e5c3e5bf1adba
seojung <ysj6115@naver.com>
2021-09-30 12:27:36 +0900

    팀원 seojung 클래스 내용이 변경됨

--- PublicGitTest/src/teammember_seojung/TeamMemberSeojung.java
+++ PublicGitTest/src/teammember_seojung/TeamMemberSeojung.java
@@ -5,7 +5,7 @@ public class TeamMemberSeojung {
 	public static void main(String[] args) {
 		
 		System.out.println("팀원 seojung 전용 브랜치에서 생성한 클래스");
-
+		System.out.println(">>> 클래스 내용이 변경됨 <<<");
 	}
 
 }
