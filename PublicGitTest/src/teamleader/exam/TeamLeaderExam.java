47b80064fd5d3c9801283c6c382ee75c210f46a2
user <user@DESKTOP-EP83E72>
2021-09-30 12:39:26 +0900

    gyeonghoon TeamLeaderExam 클래스에서 생성함

---
+++ PublicGitTest/src/teamleader/exam/TeamLeaderExam.java
@@ -0,0 +1,10 @@
+package teamleader.exam;
+
+public class TeamLeaderExam {
+
+	public static void main(String[] args) {
+		// 팀장 전용 브랜치인 gyeonghoon 브랜치에서 작업함
+		System.out.println("팀장이 작업함.");
+	}
+
+}
