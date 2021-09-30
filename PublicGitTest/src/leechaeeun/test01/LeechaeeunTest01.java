b02fdf0ab0b6aeac4907f34022401597cb57838b
coldsubak <maudlin965@naver.com>
2021-09-30 11:23:23 +0900

    팀원 이채은이 자바소스파일와 html파일을 생성

---
+++ PublicGitTest/src/leechaeeun/test01/LeechaeeunTest01.java
@@ -0,0 +1,11 @@
+package leechaeeun.test01;
+
+public class LeechaeeunTest01 {
+
+	public static void main(String[] args) {
+		// 팀원 이채은이 작업한 것임.
+		System.out.println("연습용 : 팀원 이채은이 작업한 것임 ");
+
+	}
+
+}
