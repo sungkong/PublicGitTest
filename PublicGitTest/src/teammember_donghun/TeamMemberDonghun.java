ce4d0ccb5c7217c006071fcc5ab6f492fdb06ac2
dongh <dongh@DESKTOP-LFLT5DJ>
2021-09-30 12:26:49 +0900

    팀원 전용 브랜치인 donghun 브랜치에서 클래스 생성함

---
+++ PublicGitTest/src/teammember_donghun/TeamMemberDonghun.java
@@ -0,0 +1,12 @@
+package teammember_donghun;
+
+public class TeamMemberDonghun {
+
+	public static void main(String[] args) {
+		// 팀원 donghun 전용 브랜치인 donghun 브랜치에서 생성한 클래스임
+		
+		System.out.println("팀원 donghun 전용 브랜치에서 생성한 클래스");
+
+	}
+
+}
