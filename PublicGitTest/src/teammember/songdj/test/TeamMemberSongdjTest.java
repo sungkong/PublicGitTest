f6982a996f6d52fca395f422a0f2ee7a50e87ef7
jitol <s20006s@naver.com>
2021-09-30 12:24:02 +0900

    팀원 songdj 클래스 내용이 변경됨

---
+++ PublicGitTest/src/teammember/songdj/test/TeamMemberSongdjTest.java
@@ -0,0 +1,11 @@
+package teammember.songdj.test;
+
+public class TeamMemberSongdjTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("팀원 songdj 전용 브런치에서 생성한 클래스");
+		System.out.println("할게많다잉");
+	}
+	
+}
