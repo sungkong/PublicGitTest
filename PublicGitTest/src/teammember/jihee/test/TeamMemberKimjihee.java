b9107a8b6b1b30f946f88d906ab8ab0fe9b4b5b6
Z._.Hee <Z._.Hee@DESKTOP-BHFRANB>
2021-09-30 12:15:51 +0900

    팀원 jihee 클래스 내용이 변경됨

--- PublicGitTest/src/teammember/jihee/test/TeamMemberKimjihee.java
+++ PublicGitTest/src/teammember/jihee/test/TeamMemberKimjihee.java
@@ -6,6 +6,7 @@ public class TeamMemberKimjihee {
 		// 팀원 jihee 전용 브랜치인 kimjihee 브랜치에서 생성한 클래스임.
 		
 		System.out.println("팀원 jihee 전용 브랜치에서 생성한 클래스");
+		System.out.println(">>> 클래스 내용이 변경됨 <<<");
 
 	}
 
