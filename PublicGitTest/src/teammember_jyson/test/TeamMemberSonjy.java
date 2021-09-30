b97200a1fd232b013e9426e658f5436375349b08
JuYeonson <jubar22222@gmail.com>
2021-09-30 12:27:30 +0900

    팀원의 클래스 내용이 변경되었다

--- PublicGitTest/src/teammember_jyson/test/TeamMemberSonjy.java
+++ PublicGitTest/src/teammember_jyson/test/TeamMemberSonjy.java
@@ -5,7 +5,7 @@ public class TeamMemberSonjy {
 	public static void main(String[] args) {
 		
 		System.out.println("팀원 손주연 전용 브랜치에서 생성한 클래스이다");
-
+		System.out.println(">>> 클래스의 내용이 변경이 되었다 <<<");
 	}
 
 }
