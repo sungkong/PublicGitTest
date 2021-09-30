3308835b5ac8a082ee95c6cd8b4eb9c93e2c2fcb
user <user@DESKTOP-EP83E72>
2021-09-30 12:32:59 +0900

    teamleader teammember 패키지 삭제함

--- PublicGitTest/src/teammember_gyeonghoon/TeamMemberGyeongHoon.java
+++
@@ -1,9 +0,0 @@
-package teammember_gyeonghoon;
-
-public class TeamMemberGyeongHoon {
-	
-	public static void main(String[] args) {
-		
-		System.out.println("팀원 GyeongHoon 전용 브랜치에서 생성한 클래스");
-	}
-}
