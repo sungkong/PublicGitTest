3aa18cb0dd11b8372a3f5285ccb0e0589e7a8248
﻿OhHyeonJi3088 <orange3088@gmail.com>
2021-09-30 13:03:05 +0900

    기존 패키지를 삭제하고, OhHyeonJi 브랜치에서 TeamMemberOhj 클래스 생성함

---
+++ PublicGitTest/src/team/member/ohj/TeamMemberOhj.java
@@ -0,0 +1,12 @@
+package team.member.ohj;
+
+public class TeamMemberOhj {
+
+	public static void main(String[] args) {
+		// 팀원인 ohj 전용 브랜치인 OhHyeonJi 브랜치에서 작업한 것임.
+		
+		System.out.println("### 팀원인 ohj 가 작업한 것 ###");
+
+	}
+
+}
