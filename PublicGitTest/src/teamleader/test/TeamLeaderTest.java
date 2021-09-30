2c0aa83707d22bc63d661cbcac6f17d1a831989c
user <user@DESKTOP-EP83E72>
2021-09-30 12:13:34 +0900

    new branch에서 새로 작성함

---
+++ PublicGitTest/src/teamleader/test/TeamLeaderTest.java
@@ -0,0 +1,10 @@
+package teamleader.test;
+
+public class TeamLeaderTest {
+
+	public static void main(String[] args) {
+		System.out.println("팀장 전용 브랜치에서 생성한 클래스임");
+		System.out.println("== 내용이 변경됨. ==");
+	}
+
+}
