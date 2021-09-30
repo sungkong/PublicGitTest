a2091449f9a8f7c870f7fde251248c9abe4126c7
coldsubak <maudlin965@naver.com>
2021-09-30 12:27:46 +0900

    팀원 leechaeeun 클래스 내용 변경됨

--- PublicGitTest/src/teammember_leechaeeun/Teammember_leechaeeun.java
+++ PublicGitTest/src/teammember_leechaeeun/Teammember_leechaeeun.java
@@ -6,6 +6,7 @@ public class Teammember_leechaeeun {
 		// 팀원 leechaeeun 전용 브랜치인 leechaeun 브랜치에서 생성한 크래스임
 		 System.out.println("팀원 leechaeeun 전용 브랜치인 leechaeun 브랜치에서 생성한 크래스임");
 
+		 System.out.println(">>클래스 내용 변경됨");
 	}
 
 }
