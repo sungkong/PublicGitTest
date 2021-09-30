3c31a53c4b8ce91fdb08da440e26c93abd2a8d61
jslee <jslee@LAPTOP-H7NHGFPU>
2021-09-30 12:27:01 +0900

    팀원 Leejs 전용브랜치에서 생성한 클래스에서 내용이 변경됨

--- PublicGitTest/src/teammember_Leejs/test/TeamMemberLeejs.java
+++ PublicGitTest/src/teammember_Leejs/test/TeamMemberLeejs.java
@@ -4,7 +4,7 @@ public class TeamMemberLeejs {
 
 	public static void main(String[] args) {
 		System.out.println(" 팀원 Leejs 전용브랜치에서 생성한 클래스 ");
-
+		System.out.println(" 팀원 Leejs 전용브랜치에서 생성한 클래스에서 내용이 변경됨 ");
 	}
 
 }
