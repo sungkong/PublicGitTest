5d5aa8261e36a0baab09d62cadb7fcb63d480bb6
82108 <82108@DESKTOP-UD3V2Q5>
2021-09-30 12:25:44 +0900

    팀원 parkjinwoo 전용 브랜치인 parkjinwoo 프렌치에서 생성한 클래스

---
+++ PublicGitTest/src/teammember_parkjinwoo/test/TeamMemberparkjinwoo.java
@@ -0,0 +1,12 @@
+package teammember_parkjinwoo.test;
+
+public class TeamMemberparkjinwoo {
+
+	public static void main(String[] args) {
+		// 팀원parkjinwoo 전용 브랜치인 parkjinwoo 프렌치에서 생성한 클래스
+		
+		 System.out.println("팀원parkjinwoo 전용 브랜치인 parkjinwoo 프렌치에서 생성한 클래스");
+
+	}
+
+}
