124a099c87ecaeb77c2b1b604ee0cb140ba427c9
coldsubak <maudlin965@naver.com>
2021-09-30 12:44:29 +0900

    팀원 leechaeeun 전용 브랜치인 leechaeun 브랜치에서 2번째 생성한 크래스임

---
+++ PublicGitTest/src/teammember_leechaeeun2/Teammember_leechaeeun2.java
@@ -0,0 +1,11 @@
+package teammember_leechaeeun2;
+
+public class Teammember_leechaeeun2 {
+
+	public static void main(String[] args) {
+		// 팀원 leechaeeun 전용 브랜치인 leechaeun 브랜치에서 생성한 크래스임
+		System.out.println("팀원 leechaeeun 전용 브랜치인 leechaeun 브랜치에서 2번째 생성한 크래스임");
+
+	}
+
+}
