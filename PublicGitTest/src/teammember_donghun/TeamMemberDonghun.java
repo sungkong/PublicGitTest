9bb4521c0714a6dea49ec486970b319eda8d06cf
dongh <dongh@DESKTOP-LFLT5DJ>
2021-09-30 12:27:29 +0900

    팀원 donghun 클래스 내용이 변경됨

--- PublicGitTest/src/teammember_donghun/TeamMemberDonghun.java
+++ PublicGitTest/src/teammember_donghun/TeamMemberDonghun.java
@@ -6,7 +6,8 @@ public class TeamMemberDonghun {
 		// 팀원 donghun 전용 브랜치인 donghun 브랜치에서 생성한 클래스임
 		
 		System.out.println("팀원 donghun 전용 브랜치에서 생성한 클래스");
-
+		
+		System.out.println(">>> 클래스 내용이 변경됨 <<<");
 	}
 
 }
