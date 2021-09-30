fdc58920029094a21f9187b9d14ea5c33f36233b
Z._.Hee <Z._.Hee@DESKTOP-BHFRANB>
2021-09-30 12:14:45 +0900

    팀원 전용 브랜치인 kimjihee 브랜치에서 클래스 생성함

---
+++ PublicGitTest/src/teammember/jihee/test/TeamMemberKimjihee.java
@@ -0,0 +1,12 @@
+package teammember.jihee.test;
+
+public class TeamMemberKimjihee {
+
+	public static void main(String[] args) {
+		// 팀원 jihee 전용 브랜치인 kimjihee 브랜치에서 생성한 클래스임.
+		
+		System.out.println("팀원 jihee 전용 브랜치에서 생성한 클래스");
+
+	}
+
+}
